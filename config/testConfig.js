69a459f8d2c653ba6c3e917695a5490b1d45e0fa
sagaratalatti <sagaratalatti@users.noreply.github.com>
2019-05-27 22:52:50 +0530

    flie-system added

--- config/testConfig.js
+++ config/testConfig.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 
 var FlightSuretyApp = artifacts.require("FlightSuretyApp");
 var FlightSuretyData = artifacts.require("FlightSuretyData");
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,23 +2,25 @@
 const FlightSuretyApp = artifacts.require("FlightSuretyApp");
 const FlightSuretyData = artifacts.require("FlightSuretyData");
 
+
 module.exports = function(deployer) {
 
     let firstAirline = '0x301b434ee40DBe964Aa5580603fEe5dde22a12C6';
     
     deployer.deploy(FlightSuretyData, firstAirline)
     .then(() => {
-        return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
-                .then(() => {
+        return deployer.deploy(FlightSuretyApp, FlightSuretyData.address);
+                /*.then(() => {
                     let config = {
                         localhost: {
-                            url: 'rinkeby.infura.io/v3/3800efec09524d28840ea8aa6c7ab203',
+                            url: 'http://127.0.0.1:7545',
                             dataAddress: FlightSuretyData.address,
                             appAddress: FlightSuretyApp.address,
                             numOracles: 50,
                         }
                     }
+                    fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
                     fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
-                }); 
+                }); */
     });
 }
