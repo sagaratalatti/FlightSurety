4404f3576cc5f81539edf665f86b90fb2d457245
Sagar Atalatti <sagaratalatti@users.noreply.github.com>
2019-05-27 19:03:20 +0530

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -12,7 +12,7 @@ module.exports = function(deployer) {
                 .then(() => {
                     let config = {
                         localhost: {
-                            url: 'rinkeby.infura.io/v3/3800efec09524d28840ea8aa6c7ab203',
+                            url: 'http://127.0.0.1:7547',
                             dataAddress: FlightSuretyData.address,
                             appAddress: FlightSuretyApp.address,
                             numOracles: 50,
